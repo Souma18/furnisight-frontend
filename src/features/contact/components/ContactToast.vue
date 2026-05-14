800f04be5af982910ce2dbf32e52dd6f82e103bb
devwithhao <hao.nguyen.itjava@gmail.com>
2026-05-14 17:21:12 +0700

    create: page contact

---
+++ src/features/contact/components/ContactToast.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AppIcon from '@shared/ui/AppIcon.vue'
+
+defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+  subtitle: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <div class="ct-toast" :class="{ show }">
+    <div class="ct-toast-icon">
+      <AppIcon name="check" :size="22" />
+    </div>
+    <div>
+      <div class="ct-toast-title">{{ title }}</div>
+      <div class="ct-toast-sub">{{ subtitle }}</div>
+    </div>
+  </div>
+</template>
